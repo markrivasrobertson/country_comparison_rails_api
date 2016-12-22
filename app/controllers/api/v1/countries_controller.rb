60bd46cffacf81457c9816eb865c13f63f2bb0a3
Mark Robertson <markrivasrobertson@yahoo.com>
2016-12-22 12:13:11 -0500

    reset back to working version of rails api in original directory

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -7,13 +7,18 @@ class Api::V1::CountriesController < ApplicationController
   end
 
   def show
-    @country = Country.find(params[:id])
+    @country = Country.find(params[:id]);
     render json: @country
   end
 
+  # def show_two
+  #   @country_1 = Country.find(params[:id_1])
+  #   @country_2 = Country.find(params[:id_2])
+  #   render json: @country_1, @country_2
+  # end
+
   def create
     @country = Country.new(country_params)
-
     if @country.save
       render json: @country, status: :created
     else
@@ -30,15 +35,18 @@ class Api::V1::CountriesController < ApplicationController
   end
 
   def destroy
-    @country.destroy
+    @post.destroy
   end
 
   private
-    def country_params
-      params.permit(:id)
-    end
 
-    def set_country
-      @country ||= Country.find(params[:id])
-    end
+
+      def country_params
+        params.permit(:id)
+      end
+
+      def set_country
+        @country ||= Country.find(params[:id])
+      end
+
   end
